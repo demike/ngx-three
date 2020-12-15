42d4c609a39f19a210ff64a3d789e5b011de99c2
001429831 <michael.derfler@engel.at>
2020-12-15 23:56:54 +0100

    chore: add chnaged generated file

--- projects/ngx-three/src/lib/generated/ThPointLightHelper.ts
+++ projects/ngx-three/src/lib/generated/ThPointLightHelper.ts
@@ -1,18 +1,24 @@
-import { Component, forwardRef, Input, Self, SkipSelf } from '@angular/core';
-import { Color, PointLight, PointLightHelper } from 'three';
-import { ThArgs } from '../ThArgs';
-import { ThObject3D } from './ThObject3D';
+import { PointLightHelper } from "three";
+import { Component } from "@angular/core";
+import { ThObject3D } from "./ThObject3D";
+import { ThArgs } from "../ThArgs";
+import { Input } from "@angular/core";
+import { SkipSelf, Self, forwardRef } from "@angular/core";
+import { PointLight } from "three";
+import { Color } from "three";
+import { Matrix4 } from "three";
+import { Object3D } from "three";
 
 @Component({
-  selector: 'th-pointLightHelper',
-  inputs: ['type', 'light', 'color', 'matrixAutoUpdate'],
-  template: '',
+  selector: "th-pointLightHelper",
+  inputs: ["type", "light", "color", "matrixAutoUpdate"],
+  template: "",
   providers: [
     { provide: ThObject3D, useExisting: forwardRef(() => ThPointLightHelper) },
   ],
 })
 export class ThPointLightHelper extends PointLightHelper {
-  @Input('matrix')
+  @Input("matrix")
   public set __matrix(test: any) {
     this.matrix = test;
   }
