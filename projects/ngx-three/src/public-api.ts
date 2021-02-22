9b8ea10e111fb9d5c3210cba3998e73e0b8c7728
demike <dev.mderf+github@gmail.com>
2021-02-22 23:58:52 +0100

    test: change export order

--- projects/ngx-three/src/public-api.ts
+++ projects/ngx-three/src/public-api.ts
@@ -8,8 +8,8 @@ export * from './lib/ThCanvas';
 
 export * from './lib/loaders/ThLoader';
 export * from './lib/loaders/LazyObject3dProxy';
-export * from './lib/loaders/ThOBJLoader';
 export * from './lib/loaders/ThGLTFLoader';
+export * from './lib/loaders/ThOBJLoader';
 
 export * from './lib/generated';
 export * from './lib/generated/ngx-three-generated.module';
