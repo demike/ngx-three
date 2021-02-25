9e00cbf4690c27b3d1cdf05aa9ccc71099211497
demike <dev.mderf+github@gmail.com>
2021-02-25 01:29:15 +0100

    remove files

--- projects/ngx-three/src/lib/generator/control_types.ts
+++
@@ -1,9 +0,0 @@
-export * from 'three/examples/jsm/controls/DeviceOrientationControls';
-
-export * from 'three/examples/jsm/controls/DragControls';
-export * from 'three/examples/jsm/controls/FirstPersonControls';
-export * from 'three/examples/jsm/controls/FlyControls';
-export * from 'three/examples/jsm/controls/OrbitControls';
-export * from 'three/examples/jsm/controls/PointerLockControls';
-export * from 'three/examples/jsm/controls/TrackballControls';
-export * from 'three/examples/jsm/controls/TransformControls';
