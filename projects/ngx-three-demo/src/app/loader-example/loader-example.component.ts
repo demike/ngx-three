c6716f58244e87d82ad15d63cf22146519b5fa68
demike <dev.mderf+github@gmail.com>
2021-04-11 12:08:24 +0200

    fix: remove unnecessary example dependency

--- projects/ngx-three-demo/src/app/loader-example/loader-example.component.ts
+++ projects/ngx-three-demo/src/app/loader-example/loader-example.component.ts
@@ -1,12 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { SimpleExampleComponent } from '../simple-example/simple-example.component';
 
 @Component({
   selector: 'app-loader-example',
   templateUrl: './loader-example.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LoaderExampleComponent extends SimpleExampleComponent {
+export class LoaderExampleComponent {
   public rotation: [x: number, y: number, z: number] = [0, 0, 0];
   public onBeforeRender(): void {
     this.rotation = [0, this.rotation[1] + 0.01, 0];
