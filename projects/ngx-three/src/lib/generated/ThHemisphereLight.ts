12e55ed32179d99e8c60a803dc41f20db165edc0
demike <dev.mderf+github@gmail.com>
2021-11-08 22:25:37 +0100

    fix: hemisphere lights wrong typing

--- projects/ngx-three/src/lib/generated/ThHemisphereLight.ts
+++ projects/ngx-three/src/lib/generated/ThHemisphereLight.ts
@@ -1,13 +1,7 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable @angular-eslint/component-selector, @angular-eslint/component-class-suffix, jsdoc/no-types, import/no-deprecated */
-import {
-  ChangeDetectionStrategy,
-  Component,
-  forwardRef,
-  Input,
-  Type,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, forwardRef, Input, Type } from '@angular/core';
 import { Color, ColorRepresentation, HemisphereLight, Vector3 } from 'three';
 import { applyValue } from '../util';
 import { ThLight } from './ThLight';
@@ -17,17 +11,11 @@ import { ThObject3D } from './ThObject3D';
   selector: 'th-hemisphereLight',
   template: '',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [
-    { provide: ThObject3D, useExisting: forwardRef(() => ThHemisphereLight) },
-  ],
+  providers: [{ provide: ThObject3D, useExisting: forwardRef(() => ThHemisphereLight) }]
 })
 export class ThHemisphereLight<
   T extends HemisphereLight = HemisphereLight,
-  TARGS extends any[] = [
-    skyColor?: ColorRepresentation,
-    groundColor?: ColorRepresentation,
-    intensity?: number
-  ]
+  TARGS extends any[] = [skyColor?: ColorRepresentation, groundColor?: ColorRepresentation, intensity?: number]
 > extends ThLight<T, TARGS> {
   public getType(): Type<HemisphereLight> {
     return HemisphereLight;
@@ -43,16 +31,13 @@ export class ThHemisphereLight<
   @Input()
   public set position(value: Vector3 | [x: number, y: number, z: number]) {
     if (this._objRef) {
-      this._objRef.position = applyValue<Vector3>(this._objRef.position, value);
+      applyValue<Vector3>(this._objRef.position, value);
     }
   }
   @Input()
   public set groundColor(value: Color | [color: ColorRepresentation]) {
     if (this._objRef) {
-      this._objRef.groundColor = applyValue<Color>(
-        this._objRef.groundColor,
-        value
-      );
+      this._objRef.groundColor = applyValue<Color>(this._objRef.groundColor, value);
     }
   }
 }
