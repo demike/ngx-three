c9fc806a8ac4d296f47b56c5c55bfbc69be837ca
demike <dev.mderf+github@gmail.com>
2021-12-22 08:45:06 +0100

    fix: events example

--- projects/ngx-three-demo/src/app/events-example/events-example.component.ts
+++ projects/ngx-three-demo/src/app/events-example/events-example.component.ts
@@ -1,22 +1,26 @@
-import { Component, SimpleChanges } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, SimpleChanges } from '@angular/core';
 import { ASSET_PATH } from '../assets';
 @Component({
   selector: 'app-events-example',
   templateUrl: './events-example.component.html',
-  styleUrls: ['./events-example.component.scss']
+  styleUrls: ['./events-example.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+
 })
 export class EventsExampleComponent {
   public changes: string[] = [];
   public rotation: [x: number, y: number, z: number] = [0, 0, 0];
   public position: [x: number, y: number, z: number] = [0, 1, 0];
   public readonly assetPath = `${ASSET_PATH}DamagedHelmet.glb`;
-  constructor() {
+  constructor(private cdref: ChangeDetectorRef) {
     window.setInterval(() => {
       this.rotation = [0, this.rotation[1] + 0.05, 0];
+      this.cdref.markForCheck();
     }, 1500);
 
     window.setInterval(() => {
       this.position = [0, (this.position[1] + 1) % 5, 0];
+      this.cdref.markForCheck();
     }, 3000);
   }
 
