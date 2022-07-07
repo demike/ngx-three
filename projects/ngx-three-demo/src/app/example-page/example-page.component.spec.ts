eb856062df2b635e0fe858a46fe08b57424b16d5
demike <dev.mderf+github@gmail.com>
2022-07-07 08:55:02 +0200

    fix: tests

--- projects/ngx-three-demo/src/app/example-page/example-page.component.spec.ts
+++ projects/ngx-three-demo/src/app/example-page/example-page.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { EditorService } from '../code/EditorService';
 
 import { ExamplePageComponent } from './example-page.component';
 
@@ -8,9 +10,10 @@ describe('ExamplePageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ExamplePageComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule],
+      providers: [EditorService],
+      declarations: [ExamplePageComponent]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ExamplePageComponent);
     component = fixture.componentInstance;
