efc19d4f0f4de10ca4810e2f0a12415c8ce9e60f
demike <dev.mderf+github@gmail.com>
2022-08-20 23:41:12 +0200

    fix: expose bind pipe

--- projects/ngx-three/src/public-api.ts
+++ projects/ngx-three/src/public-api.ts
@@ -35,6 +35,7 @@ export * from './lib/pipes/clone.pipe';
 export * from './lib/pipes/color.pipe';
 export * from './lib/pipes/vector.pipe';
 export * from './lib/pipes/fog.pipe';
+export * from './lib/pipes/bind.pipe';
 
 export * from './lib/stats/stats.directive';
 
