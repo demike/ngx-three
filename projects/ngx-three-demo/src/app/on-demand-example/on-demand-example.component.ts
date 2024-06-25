0e8d781f666ccf9428a68227f21f09feca900d97
demike <dev.mderf+github@gmail.com>
2024-06-25 23:30:14 +0200

    chore: revert test changes

--- projects/ngx-three-demo/src/app/on-demand-example/on-demand-example.component.ts
+++ projects/ngx-three-demo/src/app/on-demand-example/on-demand-example.component.ts
@@ -1,6 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ASSET_PATH } from '../assets';
-import { TransformControlsMode } from 'three/examples/jsm/controls/TransformControls';
 
 @Component({
   selector: 'app-on-demand-example',
@@ -9,17 +8,8 @@ import { TransformControlsMode } from 'three/examples/jsm/controls/TransformCont
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class OnDemandExampleComponent {
-  protected controlType = 'transform';
+  protected controlType = 'orbit';
   protected renderOnDemand = true;
   protected selected = false;
-  protected mode: TransformControlsMode = 'translate';
   public readonly glbPath = `${ASSET_PATH}DamagedHelmet.glb`;
-
-  public changeMode() {
-    if (this.mode === 'translate') {
-      this.mode = 'rotate';
-    } else if (this.mode === 'rotate') {
-      this.mode = 'translate';
-    }
-  }
 }
