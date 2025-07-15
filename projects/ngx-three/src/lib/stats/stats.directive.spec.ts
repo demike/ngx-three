24a065a12ea81504480073381000487dd28edf44
demike <dev.mderf+github@gmail.com>
2025-07-15 12:01:56 +0200

    fix: tests

--- projects/ngx-three/src/lib/stats/stats.directive.spec.ts
+++ projects/ngx-three/src/lib/stats/stats.directive.spec.ts
@@ -1,8 +1,29 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { StatsDirective } from './stats.directive';
+import { ThCanvas } from 'ngx-three';
+
+@Component({
+  template: `<th-canvas [thStats]></th-canvas>`,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: false,
+})
+class TestHostComponent {}
 
 describe('StatsDirective', () => {
+  let fixture: ComponentFixture<TestHostComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [StatsDirective, TestHostComponent, ThCanvas],
+    });
+    fixture = TestBed.createComponent(TestHostComponent);
+    fixture.detectChanges();
+  });
+
   it('should create an instance', () => {
-    const directive = new StatsDirective(null as any, null as any);
+    const directive = fixture.debugElement.query(By.directive(StatsDirective)).injector.get(StatsDirective);
     expect(directive).toBeTruthy();
   });
 });
