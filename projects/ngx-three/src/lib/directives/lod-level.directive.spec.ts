7ced8e896062835008360f9a1e4d950ce871f429
Eric Leibenguth <eric.leibenguth@gmail.com>
2025-11-10 11:38:28 +0100

    Implement [lodLevel] directive

---
+++ projects/ngx-three/src/lib/directives/lod-level.directive.spec.ts
@@ -0,0 +1,64 @@
+import { Component, ChangeDetectionStrategy, Input } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+import { LODLevelDirective } from "./lod-level.directive";
+import { ThLOD, ThObject3D } from "../generated";
+import { LOD, Scene } from "three";
+
+@Component({
+  template: `
+  <th-lOD>
+    <th-object3D [lodLevel]/>
+    <th-object3D [lodLevel]="{distance: 5}"/>
+    <th-object3D [lodLevel]="{distance: bar}" *ngIf="foo"/>
+  </th-lOD>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: false,
+})
+class TestHostComponent {
+  @Input() foo = true;
+  @Input() bar = 10;
+}
+
+describe('lodLevel directive', () => {
+  let fixture: ComponentFixture<TestHostComponent>;
+  let lod: LOD;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [CommonModule],
+      declarations: [TestHostComponent, LODLevelDirective, ThObject3D, ThLOD],
+      providers: [
+        {provide: ThObject3D, useValue: {objRef: new Scene()}},
+      ],
+    });
+    fixture = TestBed.createComponent(TestHostComponent);
+    fixture.detectChanges();
+    const thLOD: ThLOD = fixture.debugElement.query(By.directive(ThLOD)).componentInstance;
+    lod = thLOD.objRef!;
+  });
+
+  it('should create an instance', () => {
+    expect(lod).toBeTruthy();
+  });
+
+  it('should have 3 levels', () => {
+    expect(lod.levels).toHaveSize(3);
+  });
+
+  it('should remove level', () => {
+    fixture.componentRef.setInput('foo', false);
+    fixture.detectChanges();
+    expect(lod.levels).toHaveSize(2);
+  });
+
+  it('should change level', () => {
+    fixture.componentRef.setInput('bar', 15);
+    fixture.detectChanges();
+    expect(lod.levels).toHaveSize(3);
+    const level = lod.levels.find(l => l.distance === 15);
+    expect(level).toBeTruthy();
+  });
+});
---
+++ projects/ngx-three/src/lib/directives/lod-level.directive.ts
@@ -0,0 +1,48 @@
+import { Directive, OnDestroy, inject, Input, OnChanges, OnInit } from "@angular/core";
+import { ThLOD, ThObject3D } from "../generated";
+
+@Directive({
+  selector: '[lodLevel]',
+  standalone: false
+})
+export class LODLevelDirective implements OnInit, OnChanges, OnDestroy {
+  lod = inject(ThLOD, {skipSelf: true});
+  target = inject(ThObject3D, {self: true});
+
+  @Input()
+  lodLevel?: { distance?: number, hysteresis?: number };
+
+  ngOnInit(): void {
+    if(!this.lodLevel) {
+      this.setLevel(); // Make sure setLevel is called even when the component has no input.
+    }
+  }
+
+  ngOnChanges(): void {
+    this.setLevel();
+  }
+
+  ngOnDestroy(): void {
+    this.removeLevel();
+  }
+
+  setLevel() {
+    const target = this.target.objRef;
+    const lod = this.lod.objRef;
+    if(lod && target) {
+      this.removeLevel();
+      lod.addLevel(target, this.lodLevel?.distance, this.lodLevel?.hysteresis)
+    }
+  }
+
+  removeLevel() {
+    const target = this.target.objRef;
+    const lod = this.lod.objRef;
+    if(lod && target) {
+      const previous = lod.levels.find(l => l.object === target);
+      if(previous) {
+        lod.removeLevel(previous.distance);
+      }
+    }
+  }
+}
